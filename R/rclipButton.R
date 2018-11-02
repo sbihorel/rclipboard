9100becdf494c301677cb77bb27ab8e89bf96c50
Eftychios Valeontis <eftychios.valeontis@orbis.com>
2018-11-02 10:56:06 +0000

    Updated clipboardjs library to fix Chrome bug. This fixes #4

--- R/rclipButton.R
+++ R/rclipButton.R
@@ -8,7 +8,7 @@ rclipButton <- function(inputId, label, clipText, icon = NULL, width = NULL)
                  `data-clipboard-text` = clipText),
     tags$script(
       sprintf(
-        'var client_%s = new Clipboard( document.getElementById("%s") );',
+        'var client_%s = new ClipboardJS( document.getElementById("%s") );',
         gsub('-', '', inputId),
         inputId
       )
