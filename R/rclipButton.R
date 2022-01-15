42590c216dd313f061416c0bd3ea0319c3cc3e8c
sbihorel <pomchip@free.fr>
2022-01-15 10:23:38 -0500

    Version 0.1.4

--- R/rclipButton.R
+++ R/rclipButton.R
@@ -1,25 +1,31 @@
-rclipButton <- function(inputId, label, clipText, icon = NULL, width = NULL, modal = FALSE)
+rclip_fun <- function(FUN = actionButton, inputId, label, clipText, modal = FALSE, ...)
 {
   tagList(
-    actionButton(inputId = inputId,
-                 label = label,
-                 icon = icon,
-                 width = width,
-                 `data-clipboard-text` = clipText),
-    if (modal){
-      tags$script(
-        sprintf(
+    FUN(
+      inputId = inputId,
+      label = label,
+      ...,
+      `data-clipboard-text` = clipText
+    ),
+    tags$script(
+      sprintf(
+        ifelse(
+          modal,
           'new ClipboardJS(".btn", { container: document.getElementById("%s") } ); ',
-          inputId
-        )
+          'new ClipboardJS(".btn", document.getElementById("%s") );'
+        ),
+        inputId
       )
-    } else {
-      tags$script(
-        sprintf(
-          'new ClipboardJS(".btn", document.getElementById("%s") );',
-          inputId
-        )
-      )
-    }
+    )
   )
+}
+
+rclipButton <- function(inputId, label, clipText, modal = FALSE, ...)
+{
+  rclip_fun(actionButton, inputId, label, clipText, modal = modal, ...)
+}
+
+rclipLink <- function(inputId, label, clipText, modal = FALSE, ...)
+{
+  rclip_fun(actionLink, inputId, label, clipText, modal = modal, ...)
 }
